532e75ed83ab2014fb4bdd37de6586b1cec958d8
danyalbabur <118133023+danyalbabur@users.noreply.github.com>
2023-02-06 01:02:59 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,13 +1,17 @@
 month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def is_leap(year):
+    year = int(input("please write a year: " ))
     return year % 4 ==0 and (year % 100 != 0 or year % 400 == 0 )
 print(is_leap(2016))
 
 def days_in_month(year, month):
+    month = int(input("write a month: "))
     if not 1 <= month <= 12: 
         return 'invalid month'
     if month == 2 and is_leap(year):
         return 29
     return month_days[month]
-print(days_in_month(2017,2))
+print(days_in_month(0,0))
+
+
